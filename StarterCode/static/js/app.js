b4f24f2cd6751cbe1fc9e1cfaf172cb8aa4e24b7
Felix Pronove <felixpronove4@gmail.com>
2021-04-03 15:34:34 -0600

    updates

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,10 @@
 // Use the D3 library to read in `samples.json`.
 d3.json("../samples.json").then((importedData) => {
     // console.log(importedData);
-    var data = importedData;
+    var names= importedData.names;
+    console.log(names);
+
+    var trace1= {
+
+}
+// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
