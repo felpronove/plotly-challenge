7454c6faf9b6653e4f13afe50f9425b4603404e4
Felix Pronove <felixpronove4@gmail.com>
2021-05-29 21:18:24 -0600

    Adding more code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -9,8 +9,10 @@ function buildMetadata(sample){
     d3.json("./samples.json").then((importedData) => {
         // console.log(importedData);
         // create variables for each aspect of the data array
-        var metadata = importedData.metadata;
+        var metadatas = importedData.metadata;
     });
+    var sample_meta= sample.filter(metadatas);
+    console.log(sample_meta);
 }
 
 // create function to build charts
@@ -18,8 +20,30 @@ function buildChart(subject){
     d3.json("./samples.json").then((importedData) => {
         // console.log(importedData);
         // create variables for each aspect of the data array
-        var samples = importedData.samples;
+        var samples = importedData.metadata;
+        var sample_array = samples.filter(sampleObject => sampleObject.id==subject)
+        var result = sample_array[0]
+        var panel = d3.select('#sample-metadata')
+        panel.html('')
+        Object.entries(result).forEach(([k,v])=>{
+            panel.append('h6').text(`${k.toUpperCase()}: ${v}`);
+        })
     });
+    // find the datapoint that matches the selected sample
+
+    // var id = samples.id;
+    // var ethnicity = samples.ethnicity;
+    // var gender = samples.gender;
+    // var age = samples.age;
+    // var location = samples.location;
+    // var bbtype = samples.bbtype;
+    // var wfreq = samples.wfreq;
+
+    // var trace1 = {
+    //     x: otu_ids
+    //     y:otu_ids.values
+    // };
+
 }
 
 // create function to update the metadata and charts when updating subject
