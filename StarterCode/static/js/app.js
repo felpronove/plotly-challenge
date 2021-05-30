6e2d3af5c11e295d76661bcf77ee1730b71e1a4a
Felix Pronove <felixpronove4@gmail.com>
2021-05-29 22:36:12 -0600

    added more information

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -10,25 +10,101 @@ function buildMetadata(sample){
         // console.log(importedData);
         // create variables for each aspect of the data array
         var metadatas = importedData.metadata;
-    });
-    var sample_meta= sample.filter(metadatas);
-    console.log(sample_meta);
+        var resultArr = metadatas.filter(sampleObject => sampleObject.id == sample)
+        var result = resultArr[0]
+        var panel = d3.select('#sample-metadata-panel')
+        panel.html('')
+        Object.entries(result).forEach(([k,v]) =>{
+            panel.append('h6').text(`${k.toUpperCase()}: ${v}`);
+        })
+    })
 }
 
 // create function to build charts
-function buildChart(subject){
+function buildChart(subject,n){
     d3.json("./samples.json").then((importedData) => {
         // console.log(importedData);
         // create variables for each aspect of the data array
-        var samples = importedData.metadata;
+        var samples = importedData.samples;
         var sample_array = samples.filter(sampleObject => sampleObject.id==subject)
         var result = sample_array[0]
-        var panel = d3.select('#sample-metadata')
-        panel.html('')
-        Object.entries(result).forEach(([k,v])=>{
-            panel.append('h6').text(`${k.toUpperCase()}: ${v}`);
+        var chart_IDs = result.otu_ids
+        var labels = result.otu_labels
+        var sampleVals= result.sample_values
+        var otu_labels = result.otu_labels
+
+        // create bubble charts
+        // set up bubble chart layout
+        var bubbleChartlayout = {
+            'title': 'Bubble Chart',
+            'margin': {
+                t:0
+            },
+            'hovermode': 'closest',
+            'margin': {t:30}
+        };
+
+        // create actual bubbles for bubble chart
+        var bubbleChart = {
+            'x': chart_IDs,
+            'y': sampleVals,
+            'text': labels,
+            'sizemode': 'area',
+            'mode': 'markers',
+            'marker':{
+                'size': sampleVals,
+                'color': chart_IDs,
+                'colorscale': 'Earth'
+            }
+        }
+
+        // create a horizontal bar chart
+        var barLayout ={
+            'title': 'Top 10 Belly Button Cultures',
+        };
+
+        new_id=[]
+        Object.entries(chart_IDs).forEach(([k,v])=>{
+            new_id.push(('otu-'+ v))
         })
-    });
+
+        var barTrace = {
+            'x': sampleVals.slice(0,10).reverse(),
+            'y': new_id.slice(0,10),
+
+            'type': 'bar',
+            'orientation': 'h',
+            'text': otu_labels
+        }
+
+        // wash = importedData['metadata'][n]["wfreq"];
+
+        // var gaugeTrace = {
+        //     domain: {x: [0,1], y: [0,1]},
+        //     type: "indicator",
+        //     mode: "gauge+number",
+
+        //     gauge: {
+        //         axis: {visible:true, range:[null,9], nticks:10},
+        //         bar: {color: 'red'},
+        //     },
+
+        //     value: wash,
+        //     title: {text: "Belly Button Washing frequency %n Scrubs per week"}
+        // }
+
+        // var gaugeLayout = {
+        //     width: 600,
+        //     height: 500,
+        //     margin: {t: 0, b: 0},
+        // };
+
+        // Add the chart to the page
+        Plotly.newPlot('bubble', [bubbleChart], bubbleChartlayout);
+        Plotly.newPlot('bar', [barChart], barChartlayout);
+        // Plotly.newPlot('gauge', [gaugeChart], gaugeChartlayout);
+
+    })
     // find the datapoint that matches the selected sample
 
     // var id = samples.id;
@@ -48,8 +124,20 @@ function buildChart(subject){
 
 // create function to update the metadata and charts when updating subject
 function optionChanged(option){
-    buildMetadata(option);
-    buildChart(option);
+    var dropdownMenu = d3.select('#selDataset');
+    var new_sample = dropdownMenu.property('value');
+    d3.json('./samples.json').then(function(data) {
+        var samples = data.samples
+        var resultArr = samples.filter(sampleObject=> sampleObject.id == option)
+
+        for (var i=0; i<153; i++){
+            if (samples[i].id==option){
+                idx = i
+            }
+        }
+        buildMetadata(option)
+        buildChart(option,idx)
+    });
 }
 
 // create function to initialize the page
